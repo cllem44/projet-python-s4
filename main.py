1586fe99526866967e0c9e36cd1ef09a781afe95
esteban-dul <esteban.dulout@reseau.eseo.fr>
2026-03-06 11:54:24 +0100

    main.py 06/03

--- main.py
+++ main.py
@@ -12,15 +12,17 @@ app = ctk.CTk()
 app.title("Prototype")
 app.geometry("400x700")
 app.resizable(width=False,height=False)
-
+frame_ecran = Frame(app)
+frame_ecran.pack(fill="both", expand=True)
+barre_bas = Frame(app, height=60, bg="#c1c1c1")
+barre_bas.pack(fill="x", side="bottom")
 #tkinter
-canvas = Canvas(app, bg="black") 
+canvas = Canvas(frame_ecran, bg="black") 
 #fill="both" : prend largeur + hauteur
 #expand=True : utilise tout l’espace disponible
 
-app.create_rectangle(0,600,400,600,fill="#c1c1c1")
-buttoneteindre = ctk.CTkButton(master=app,text="Eteindre",command=Eteindretelephone,width=80,height=40,border_width=0,corner_radius=4,hover=False)
+buttoneteindre = ctk.CTkButton(master=barre_bas,text="Eteindre",command=Eteindretelephone,width=80,height=40,border_width=0,corner_radius=4,hover=False)
 buttoneteindre.pack(side="bottom")
-buttonprincipale = ctk.CTkButton(master=app,text="",command=Ecranaccueil,width=55,height=55,border_width=0,corner_radius=30,hover=False)
+buttonprincipale = ctk.CTkButton(master=barre_bas,text="",command=Ecranaccueil,width=55,height=55,border_width=0,corner_radius=30,hover=False)
 buttonprincipale.pack(side="bottom")
 app.mainloop()
