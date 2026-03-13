4c62a1eb97092dcddb55c85c10ca7066a01dffa0
cllem44 <clement.faucher2402@gmail.com>
2026-03-13 18:06:46 +0100

    bla

--- application/meteo.py
+++ application/meteo.py
@@ -1,5 +1,8 @@
 from meteofrance_api import MeteoFranceClient
 from datetime import datetime
+import customtkinter as ctk
+from tkinter import *
+
 
 def API_meteo_test():
     # 1. Initialisation du client
@@ -45,7 +48,38 @@ def API_meteo_test():
         print(f"Le {date} -> Min: {t_min}°C, Max: {t_max}°C - {desc}")
     
 
-   
+def afficher_valeur():
+    # 1. On récupère la valeur du champ
+    valeur_saisie = Ville.get() 
+    
+    # 2. On modifie le texte du Label qui se trouve dans la Frame
+    affiche_ville.config(ville=f" {valeur_saisie}")
+
+#customtkinter
+app = ctk.CTk()
+app.title("Prototype")
+app.geometry("400x700")
+app.iconbitmap("img/logo.ico")
+app.resizable(width=False,height=False)
+app.grid_rowconfigure(0, weight=1)  # écran principal
+app.grid_rowconfigure(1, weight=0)  # barre du bas
+app.grid_columnconfigure(0, weight=1)    
+
+frame_meteo = Frame(app)
+
+ville="Angers"
+affiche_ville = Label( text = ville, fg='#FFFFFF', bg='#3396ff')
+affiche_ville.grid(column=0,row=0,sticky="n",pady=6)
+
+Ville= Entry( bg="white", font="Courier", bd=5, justify=CENTER, textvariable =ville)
+Ville.grid(column=0,row=1 ,sticky="n",pady=6)
+
+bouton_valider = Button( text="Valider", command=afficher_valeur)
+bouton_valider.grid(column=0,row=2 ,sticky="n",pady=6)
+
+
+app.mainloop()
+
 
 if __name__ == "__main__":
-    API_meteo_test()
+    print(ville)
