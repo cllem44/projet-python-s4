aa2753590c7b53c7e55126c0286dd73d208e6794
ArthurChst <arthur.choisnet@reseau.eseo.fr>
2026-03-13 09:48:28 +0100

    début appli horloge

--- application/horloge.py
+++ application/horloge.py
@@ -1,1 +1,13 @@
+import datetime as dt
+from tkinter import *
 
+appli_horloge = Tk()
+appli_horloge.title("Horloge")
+appli_horloge.iconbitmap("img/logo.ico")
+appli_horloge.geometry("400x640")
+appli_horloge.config(background="#000000")
+
+
+
+
+appli_horloge.mainloop()
